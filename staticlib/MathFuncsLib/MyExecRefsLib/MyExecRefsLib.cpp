91d9092cbb1801ed513d864b153929df495cf4b5
NJUPTB18150219LF <55730907+NJUPTB18150219LF@users.noreply.github.com>
2019-11-26 13:52:13 +0800

    静态库实现与调用

---
+++ staticlib/MathFuncsLib/MyExecRefsLib/MyExecRefsLib.cpp
@@ -0,0 +1,23 @@
+﻿// MyExecRefsLib.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+#include <iostream>
+
+#include "MathFuncsLib.h"
+
+using namespace std;
+
+int main()
+{
+	double a = 7.4;
+	int b = 99;
+
+	cout << "a + b = " <<
+		MathFuncs::MyMathFuncs::Add(a, b) << endl;
+	cout << "a - b = " <<
+		MathFuncs::MyMathFuncs::Subtract(a, b) << endl;
+	cout << "a * b = " <<
+		MathFuncs::MyMathFuncs::Multiply(a, b) << endl;
+	cout << "a / b = " <<
+		MathFuncs::MyMathFuncs::Divide(a, b) << endl;
+
+	return 0;
+}
